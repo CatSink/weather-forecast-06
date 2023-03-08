b0fbb195d8f51bd043ad41d9a1a24c8e002876a5
CatSink <raindropwebweaver@live.com>
2023-03-07 21:52:37 -0500

    added function to call geo API for city coordinates, added function to add history to local storage, and ids for results container

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,49 @@
 //add curl request for geographic lat and long coordinates for cities; then get curl for weather forecast
+var apiKey = '0a11094e258df57717907ca4f7fdba17';
+var userFormEl = document.querySelector('#user-form');
+//selects the form element for user input/search
+var nameInputEl = document.querySelector('#city-name');
+//selects the input for user name
+var resultsContainerEl = document.querySelector('#results-container');
+var currentCity;
+
+var getCoordinates = function () {
+  var apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&appid=${apiKey}`;
+  var searchHistory= JSON.parse(localStorage.getItem('city-search-history')) || [];  
+ //function for saving user search history and getting city coordinates
+  fetch(apiUrl)
+    .then(function (response) {
+        if (response.statusCode === 200 && response.statusCode <=299) {
+            return response.json();
+        } else { 
+          throw new Error(response.statusText);
+        }
+    })
+     .then(function (data) {        
+      var convertCity = {
+        city: currentCity,
+        lat: data.coord.lat,
+        lon: data.coord.lon
+      };
+    searchHistory.push(convertCity);
+    localStorage.setItem('cities', JSON.stringify(searchHistory));
+
+    displaySearchHistory();
+
+    return convertCity;
+    })
+    .then(function (data) {
+        getWeather(data);
+    });
+    return;
+}
+function getForecast(data) {
+    var requestUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(requestUrl)
+     .then(function (data) {
+        return response.json();
+
+     });
+} 
+ //need to select content to import from API data, temp, humidity, wind, and icon    
+      
